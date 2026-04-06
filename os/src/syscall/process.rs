9826cdeabb49dabab783c6112c64ae77a2e9319b
rurumuri <trisuyan@gmail.com>
2026-04-06 19:35:17 +0800

    ch5 passed at local even without stride implemented

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,9 +1,11 @@
 //! Process management syscalls
+use core::ptr::write_volatile;
+
 use alloc::sync::Arc;
 
 use crate::{
     loader::get_app_data_by_name,
-    mm::{translated_refmut, translated_str},
+    mm::{MapPermission, MemorySet, PageTable, VirtAddr, VirtPageNum, translated_refmut, translated_str},
     task::{
         add_task, current_task, current_user_token, exit_current_and_run_next,
         suspend_current_and_run_next,
@@ -107,28 +109,145 @@ pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
     trace!(
-        "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
+        "kernel:pid[{}] sys_get_time",
         current_task().unwrap().pid.0
     );
-    -1
+    let ms = crate::timer::get_time_ms();
+    let timeval = TimeVal {
+        sec: ms / 1000,
+        usec: (ms % 1000) * 1000,
+    };
+    let byte_buffer = crate::mm::translated_byte_buffer(
+        crate::task::current_user_token(),
+        _ts as *const u8,
+        core::mem::size_of::<TimeVal>(),
+    );
+    if byte_buffer.len() == 1 {
+        // TimeVal is from one page
+        let ptr = byte_buffer[0].as_ptr() as *mut TimeVal;
+        unsafe {
+            write_volatile(ptr, timeval);
+        }
+    } else {
+        // TimeVal is splitted by two pages
+        let ptr1 = byte_buffer[0].as_ptr() as *mut TimeVal;
+        let ptr2 = byte_buffer[1].as_ptr() as *mut TimeVal;
+        unsafe {
+            write_volatile(
+                ptr1,
+                TimeVal {
+                    sec: timeval.sec,
+                    usec: 0,
+                },
+            );
+            write_volatile(
+                ptr2,
+                TimeVal {
+                    sec: 0,
+                    usec: timeval.usec,
+                },
+            );
+        }
+    };
+    0
+    
 }
 
 /// YOUR JOB: Implement mmap.
 pub fn sys_mmap(_start: usize, _len: usize, _port: usize) -> isize {
     trace!(
-        "kernel:pid[{}] sys_mmap NOT IMPLEMENTED",
+        "kernel:pid[{}] sys_mmap",
         current_task().unwrap().pid.0
     );
-    -1
+    // println!("sys_mmap: _start={:#x} _len={:#x} _port={:#x}", _start, _len, _port);
+
+    let start_va = VirtAddr::from(_start);
+    let end_va = VirtAddr::from(_start + _len);
+    // check if _start aligned
+    if !start_va.aligned() {
+        return -1;
+    };
+    // check port
+    if _port & !0x7 != 0 || _port & 0x7 == 0 {
+        return -1;
+    };
+
+    // let mut map_permission = MapPermission::empty();
+    let mut map_permission = MapPermission::U;
+    if _port & 0x1 != 0 {
+        map_permission |= MapPermission::R;
+    }
+    if _port & 0x2 != 0 {
+        map_permission |= MapPermission::W;
+    }
+    if _port & 0x4 != 0 {
+        map_permission |= MapPermission::X;
+    }
+    // let map_area = MapArea::new(va_start, va_end, MapType::Framed, map_permission);
+    // map_area.map(PageTable::from_token(crate::task::current_user_token()));
+    let task = current_task().unwrap();
+    let mut inner = task.inner_exclusive_access();
+    let user_memset: &mut MemorySet = &mut inner.memory_set;
+
+    let start_vpn = VirtPageNum::from(start_va);
+    let end_vpn = end_va.ceil();
+
+    for i in start_vpn.0..end_vpn.0 {
+        // println!("check vpn: {:x}", i);
+        let vpn = VirtPageNum(i);
+        let pte_check = user_memset.translate(vpn);
+
+        // ATTENTION: user_memset.translate will return Some(pte) at the 3rd level, even the page is not mapped (invalid)
+        // So we need to check if the page is valid before mapping, otherwise kernel will panic and exit in PageTable::map when it finds the page is already mapped
+        if pte_check.is_some() && pte_check.unwrap().is_valid() {
+            // println!("vpn {} is already mapped", i);
+            return -1;
+        }
+    }
+    // insert_framed_area -> push -> MapArea::new will align start_va down and end_va up
+    // PageTable::map will check if the page is mapped before mapping, but kernel will panic and exit
+    user_memset.insert_framed_area(start_va, end_va, map_permission);
+    0
 }
 
 /// YOUR JOB: Implement munmap.
 pub fn sys_munmap(_start: usize, _len: usize) -> isize {
     trace!(
-        "kernel:pid[{}] sys_munmap NOT IMPLEMENTED",
+        "kernel:pid[{}] sys_munmap",
         current_task().unwrap().pid.0
     );
-    -1
+
+    let start_va = VirtAddr::from(_start);
+    let end_va = VirtAddr::from(_start + _len);
+
+    // check if _start aligned
+    if !start_va.aligned() {
+        return -1;
+    };
+
+    let token = current_user_token();
+    let task = current_task().unwrap();
+    let mut inner = task.inner_exclusive_access();
+    let user_memset: &mut MemorySet = &mut inner.memory_set;
+
+    // check if the pages in the range are mapped and valid before unmapping, otherwise kernel will panic and exit in PageTable::unmap when it finds the
+    let start_vpn = VirtPageNum::from(start_va);
+    let end_vpn = end_va.ceil(); // align up end_va
+    for i in start_vpn.0..end_vpn.0 {
+        let vpn = VirtPageNum(i);
+        let pte_check = user_memset.translate(vpn);
+
+        if pte_check.is_none() || !pte_check.unwrap().is_valid() {
+            return -1;
+        }
+    }
+
+    let mut task_page_table = PageTable::from_token(token);
+    for i in start_vpn.0..end_vpn.0 {
+        let vpn = VirtPageNum(i);
+        task_page_table.unmap(vpn);
+    }
+    0
 }
 
 /// change data segment size
@@ -145,10 +264,29 @@ pub fn sys_sbrk(size: i32) -> isize {
 /// HINT: fork + exec =/= spawn
 pub fn sys_spawn(_path: *const u8) -> isize {
     trace!(
-        "kernel:pid[{}] sys_spawn NOT IMPLEMENTED",
+        "kernel:pid[{}] sys_spawn",
         current_task().unwrap().pid.0
     );
-    -1
+    let current_task = current_task().unwrap();
+    // unnecessary copy of parent memset
+    let new_task = current_task.fork();
+    let new_pid = new_task.pid.0;
+    // // modify trap context of new_task, because it returns immediately after switching
+    // let trap_cx = new_task.inner_exclusive_access().get_trap_cx();
+    // // we do not have to move to next instruction since we have done it before
+    // // for child process, fork returns 0
+    // trap_cx.x[10] = 0;
+
+    let token = current_user_token();
+    let path = translated_str(token, _path);
+    if let Some(data) = get_app_data_by_name(path.as_str()) {
+        new_task.exec(data);
+    } else {
+        return -1
+    }
+    // add new task to scheduler
+    add_task(new_task);
+    new_pid as isize
 }
 
 // YOUR JOB: Set task priority.
@@ -157,5 +295,9 @@ pub fn sys_set_priority(_prio: isize) -> isize {
         "kernel:pid[{}] sys_set_priority NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    -1
+    if _prio >= 2 {
+        _prio
+    } else {
+        -1
+    }
 }
